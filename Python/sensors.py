4bc8d54f9a9d5ee1e4bc712b79d325df365272a3
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2023-11-16 00:42:34 -0600

    added camera class to sensors

--- Python/sensors.py
+++ Python/sensors.py
@@ -1,3 +1,22 @@
+import cv2
+
+class Camera:
+
+    STATIC_CAM = 0
+    ARM_CAM = 1
+
+    def get_image(camera_id):
+        """returns a single image from the camera
+
+        Args:
+            camera_id (int): id of the camera to use
+
+        Returns:
+            numpy.uint8: image from the camera
+        """
+        with cv2.VideoCapture(camera_id) as cap:
+            _, frame = cap.read()
+            return frame
 
 class Phototransistor:
     def detect_start_signal():
