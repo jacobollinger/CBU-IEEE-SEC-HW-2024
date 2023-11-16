a455eae53842cc02e5dffbfb6c887b589855e653
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2023-11-16 00:52:29 -0600

    added a timer class

---
+++ Python/timer.py
@@ -0,0 +1,35 @@
+import time
+
+class Timer:
+    def __init__(self):
+        self.start_time = 0
+        self.end_time = 0
+        self.running = False
+
+    def start(self):
+        self.start_time = time.time()
+        self.running = True
+
+    def stop(self):
+        self.end_time = time.time()
+        self.running = False
+
+    def get_time(self):
+        if self.running:
+            return time.time() - self.start_time
+        else:
+            return self.end_time - self.start_time
+
+    def __str__(self):
+        minutes, seconds = divmod(self.get_time(), 60)
+        return f"Time Taken: {int(minutes):02}:{seconds:06.3f}"
+    
+if __name__ == "__main__":
+    timer = Timer()
+    print(timer)
+    timer.start()
+    while timer.get_time() < 10:
+        time.sleep(1)
+        print(timer)
+    timer.stop()
+    print(timer)
