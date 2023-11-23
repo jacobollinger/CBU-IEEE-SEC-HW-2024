9642a0b1dd8e00b61b66801be861dbfaabb8f22c
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2023-11-22 21:55:04 -0600

    updated sensors file

--- Python/sensors.py
+++ Python/sensors.py
@@ -1,9 +1,11 @@
 import cv2
+from gpio import GPIO
+import time
 
 class Camera:
 
-    STATIC_CAM = 0
-    ARM_CAM = 1
+    STATIC_CAM_PIN = 0 # TODO: change this to the correct pin
+    ARM_CAM_PIN = 0 # TODO: change this to the correct pin
 
     def get_image(camera_id):
         """returns a single image from the camera
@@ -19,6 +21,37 @@ class Camera:
             return frame
 
 class Phototransistor:
+    PIN = 0 # TODO: change this to the correct pin
+    
     def detect_start_signal():
         # TODO: returns true if the green LED is on
         pass
+    
+class Ultrasonic:
+    # Pins: [trigger, echo]
+    FRONT = [0, 0] # TODO: change this to the correct pin
+    BACK = [0, 0] # TODO: change this to the correct pin
+    LEFT = [0, 0] # TODO: change this to the correct pin
+    RIGHT = [0, 0] # TODO: change this to the correct pin
+    
+    def get_distance(pins):
+            GPIO.output(pins[0], True)
+
+            time.sleep(0.00001)
+            GPIO.output(pins[0], False)
+        
+            start = time.time()
+            end = time.time()
+
+            while GPIO.input(pins[1]) == 0:
+                start = time.time()
+
+            while GPIO.input(pins[1]) == 1:
+                end = time.time()
+
+            time_elapsed = end - start
+            
+            # speed of sound: 34300 cm/s
+            distance = (time_elapsed * 34300) / 2
+        
+            return distance
