2c72d0c2aff802e9a4e13c2678045596e99861ce
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2023-11-24 14:51:05 -0600

    Fix import statement in model_train.py

--- Python/model_train.py
+++ Python/model_train.py
@@ -3,7 +3,7 @@ import time
 import torch
 from torch.utils.data import DataLoader, random_split
 
-from .ml import GameObjectDataset, GameObjectModel
+from ml import GameObjectDataset, GameObjectModel
 
 start_time = time.time()
 
