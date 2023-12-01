4cd5ac9cb0e80aa81e2376f83586f46674d59e74
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2023-12-01 17:12:01 -0600

    organized the repo

--- Python/data_preprocessing/generate_images.py
+++ Python/data_preprocessing/generate_images.py
@@ -1,9 +1,7 @@
-import csv
 import ffmpeg
 import os
-import subprocess
 
-fps = 10
+fps = 1
 
 def strip_video(path):
     if os.path.isdir(path):
@@ -26,27 +24,5 @@ def strip_video(path):
         
         ffmpeg.input(path).output(f"./data/in/images/raw/{type}/{basename}_%d.jpg", vf=f"fps={fps}", pix_fmt="yuvj420p").run()
 
-def generate_annotations():
-    type_to_number = {
-        "small_package": 0,
-        "thruster": 1,
-        "large_package": 2,
-        "fuel_tank_thruster_assembly": 3,
-        "fuel_tank": 4,
-    }
-
-    with open("./data/in/annotations2.csv", "w") as f:
-        writer = csv.writer(f)
-        for root, dirs, files in os.walk("./data/in/images/raw"):
-            for file in files:
-                if file == ".DS_Store":
-                    continue
-                path = os.path.join(root, file)
-                type = os.path.basename(os.path.dirname(path))
-                number = type_to_number.get(type)
-                print(f"{file} => {type} ({number})")
-                writer.writerow([file, type, number])
-
 if __name__ == "__main__":
-    # strip_video("./data/in/videos/processed")
-    generate_annotations()
+    strip_video("./data/in/videos")
