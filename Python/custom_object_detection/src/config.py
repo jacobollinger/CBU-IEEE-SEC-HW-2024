0b2578b74464cedd9e38511a37f92f9f392b604f
Abdala-Jabase <abdala.jabase@gmail.com>
2023-12-01 14:55:31 -0600

    Custom Object Detection Model made

---
+++ Python/custom_object_detection/src/config.py
@@ -0,0 +1,21 @@
+import torch
+
+BATCH_SIZE = 16 # Increase / decrease according to GPU memeory.
+RESIZE_TO = 640 # Resize the image for training and transforms.
+NUM_EPOCHS = 10 # Number of epochs to train for.
+NUM_WORKERS = 4 # Number of parallel workers for data loading.
+
+DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu"
+# training images and XML files directory
+TRAIN_DIR = './data/Microcontroller_Detection/train'
+# validation images and XML files directory
+VALID_DIR = './data/Microcontroller_Detection/test'
+# classes: 0 index is reserved for background
+CLASSES = [
+    'background', 'Arduino_Nano', 'ESP8266', 'Raspberry_Pi_3', 'Heltec_ESP32_Lora'
+]
+NUM_CLASSES = len(CLASSES)
+# whether to visualize images after crearing the data loaders
+VISUALIZE_TRANSFORMED_IMAGES = False
+# location to save model and plots
+OUT_DIR = './Python/custom_object_detection/outputs'
