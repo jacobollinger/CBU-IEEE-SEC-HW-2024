f194a1f044b5d0c075f24a3f17c89130aae14d95
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2023-12-01 18:41:59 -0600

    added scripts to setup.py

--- Python/data_preprocessing/decompress_data.py
+++ Python/data_preprocessing/decompress_data.py
@@ -1,7 +1,12 @@
 from zipfile import ZipFile
 
-with ZipFile("./data_in.zip", 'r') as zip_ref:
-    zip_ref.extractall("./data/in/videos")
-    
-with ZipFile("./data_out.zip", 'r') as zip_ref:
-    zip_ref.extractall("./data/out/models")
+
+def decompress_data():
+    with ZipFile("./data_in.zip", 'r') as zip_ref:
+        zip_ref.extractall("./data/in/videos")
+        
+    with ZipFile("./data_out.zip", 'r') as zip_ref:
+        zip_ref.extractall("./data/out/models")
+
+if __name__ == "__main__":
+    decompress_data()
