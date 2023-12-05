e4a4542085d18e0f9882e4b5720ae8c16c7f820f
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2023-12-05 15:45:37 -0600

    fixed fps test script

--- Python/modules/model/fps_test.py
+++ Python/modules/model/fps_test.py
@@ -1,20 +1,23 @@
+import random
 import time
 import torch
 
-from ml import GameObjectModel
+from ml import GameObjectDataset, GameObjectModel
 
 with torch.no_grad():
+    dataset = GameObjectDataset()
     model = GameObjectModel()
     
     start_time = previous_time = time.time()
 
-    iterations = 1000
+    iterations = 100
     for i in range(iterations):
-        output = model(torch.rand(1, 3, 224, 224).to(model.DEVICE))
+        rand = random.randint(0, len(dataset) - 1)
+        output = model(dataset[rand][0].unsqueeze(0))
         current_time = time.time()
         if i % 10 == 0:
             print(f"FPS: {1 / (current_time - previous_time):4.2f}\tProgress: {(i / iterations * 100):5.2f}%")
-            
+
         previous_time = current_time
         
     end_time = time.time()
