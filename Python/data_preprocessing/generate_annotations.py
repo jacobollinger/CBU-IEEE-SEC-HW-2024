31bedc8e82372ecfc082817cc0b892c8c0521be3
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2023-12-05 01:47:03 -0600

    Update data preprocessing scripts

--- Python/data_preprocessing/generate_annotations.py
+++ Python/data_preprocessing/generate_annotations.py
@@ -1,26 +1,22 @@
 import csv
 import os
 
-def generate_annotations():
-    type_to_number = {
-        "small_package": 0,
-        "thruster": 1,
-        "large_package": 2,
-        "fuel_tank_thruster_assembly": 3,
-        "fuel_tank": 4,
-    }
+from Python.modules.model.ml import GameObjectModel
 
-    with open("./data/in/annotations.csv", "w") as f:
+def generate_annotations():
+    print("Generating annotations...")
+    object_class_to_number = {c: n for n, c in enumerate(GameObjectModel.CLASSES)}
+
+    with open("./data/in/annotations.csv", "w", newline='') as f:
         writer = csv.writer(f)
-        for root, dirs, files in os.walk("./data/in/images/raw"):
+        for root, _, files in os.walk("./data/in/images/raw"):
             for file in files:
                 if file == ".DS_Store":
                     continue
-                path = os.path.join(root, file)
-                type = os.path.basename(os.path.dirname(path))
-                number = type_to_number.get(type)
-                print(f"{file} => {type} ({number})")
-                writer.writerow([file, type, number])
+                object_class = os.path.basename(root)
+                number = object_class_to_number[object_class]
+                # print(f"{file} => {object_class} ({number})")
+                writer.writerow([file, object_class, number])
 
 if __name__ == "__main__":
     generate_annotations()
--- setup.py
+++ setup.py
@@ -5,22 +5,15 @@ from Python.data_preprocessing.decompress_data import decompress_data
 from Python.data_preprocessing.generate_images import generate_images
 from Python.data_preprocessing.generate_annotations import generate_annotations
 
-# get data:
-print("Downloading data...")
-download_data(
-    "https://mega.nz/file/ITs1gACR#3W8t6PR2inOByLBI5450d9ebAcd06_9eCOY24p8QCLA",
-    "https://mega.nz/file/VT9WQSaA#XGYL9DlEbiCd8HwrLdyTT8uhP_H-X58JYmlclGQKyHE",
-)
+if not os.path.exists("./data/data_in.zip"):
+    download_data(data_out=False)
+if not os.path.exists("./data/data_out.zip"):
+    download_data(data_in=False)
 
-print("Decompressing data...")
-decompress_data()
+# decompress_data()
 
-print("Generating images...")
-generate_images(fps=10)
+generate_images(fps=None)
 
-print("Generating annotations...")
 generate_annotations()
 
-# os.remove("data_in.zip")
-# os.remove("data_out.zip")
 print("Setup complete!")
