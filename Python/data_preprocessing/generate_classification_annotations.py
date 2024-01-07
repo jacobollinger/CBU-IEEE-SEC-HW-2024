4467d306fe5878b40a81f3b0cb20a8bbde601bd1
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-01-06 18:36:56 -0600

    Update annotation file name in data preprocessing script

--- Python/data_preprocessing/generate_classification_annotations.py
+++ Python/data_preprocessing/generate_classification_annotations.py
@@ -7,7 +7,7 @@ def generate_classification_annotations():
     print("Generating annotations...")
     object_class_to_number = {c: n for n, c in enumerate(GameObjectModel.CLASSES)}
 
-    with open("./data/in/annotations.csv", "w", newline='') as f:
+    with open("./data/in/classification_annotations.csv", "w", newline='') as f:
         writer = csv.writer(f)
         for root, _, files in os.walk("./data/in/images/raw"):
             for file in files:
