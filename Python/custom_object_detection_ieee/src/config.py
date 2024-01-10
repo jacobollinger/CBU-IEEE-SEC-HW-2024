f0475506f26a756eccbff69409bdb31f9fd5a70f
Abdala Jabase <abdala.jabase@gmail.com>
2024-01-10 14:27:57 -0600

    made ieee custom model

--- Python/custom_object_detection_ieee/src/config.py
+++ Python/custom_object_detection_ieee/src/config.py
@@ -10,15 +10,15 @@ NUM_WORKERS = 4 # Number of parallel workers for data loading.
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # training images and XML files directory
-TRAIN_DIR = './data/in/Microcontroller_Detection/train'
+TRAIN_DIR = './data/in/IEEE/test'
 # validation images and XML files directory
-VALID_DIR = './data/in/Microcontroller_Detection/test'
+VALID_DIR = './data/in/IEEE/test'
 # classes: 0 index is reserved for background
 CLASSES = [
-    'background', 'Arduino_Nano', 'ESP8266', 'Raspberry_Pi_3', 'Heltec_ESP32_Lora'
+    'background', 'small_package', 'large_package', 'fuel_tank', 'thruster','small_package_delivery_zone','large_package_delivery_zone','fuel_tank_pickup_zone','crater_line'
 ]
 NUM_CLASSES = len(CLASSES)
 # whether to visualize images after crearing the data loaders
 VISUALIZE_TRANSFORMED_IMAGES = False
 # location to save model and plots
-OUT_DIR = './Python/custom_object_detection/outputs'
+OUT_DIR = './Python/custom_object_detection_ieee/outputs'
