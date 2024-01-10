84cc47e85064a7536bdeaf3c697fa375ac544009
Abdala Jabase <abdala.jabase@gmail.com>
2024-01-10 14:27:57 -0600

    made ieee custom model

---
+++ Python/custom_object_detection_ieee/src/model.py
@@ -0,0 +1,59 @@
+import torchvision
+import torch.nn as nn
+
+from torchvision.models.detection.ssd import (
+    SSD, 
+    DefaultBoxGenerator,
+    SSDHead
+)
+#RESNET 34
+#Average FPS: 69.778
+
+#RESNET 50
+#Average FPS: 51
+
+def create_model(num_classes=91, size=300, nms=0.45):
+    model_backbone = torchvision.models.resnet18(
+        weights=torchvision.models.ResNet18_Weights.DEFAULT
+    )
+
+    conv1 = model_backbone.conv1
+    bn1 = model_backbone.bn1
+    relu = model_backbone.relu
+    max_pool = model_backbone.maxpool
+    layer1 = model_backbone.layer1
+    layer2 = model_backbone.layer2
+    layer3 = model_backbone.layer3
+    layer4 = model_backbone.layer4
+    backbone = nn.Sequential(
+        conv1, bn1, relu, max_pool, 
+        layer1, layer2, layer3, layer4
+    )
+    out_channels = [512, 512, 512, 512, 512, 512]
+    anchor_generator = DefaultBoxGenerator(
+        [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    )
+    num_anchors = anchor_generator.num_anchors_per_location()
+    head = SSDHead(out_channels, num_anchors, num_classes)
+    model = SSD(
+        backbone=backbone,
+        num_classes=num_classes,
+        anchor_generator=anchor_generator,
+        size=(size, size),
+        head=head,
+        nms_thresh=nms
+    )
+    return model
+
+if __name__ == '__main__':
+    model = create_model(2, 300)
+    print(model)
+    # Total parameters and trainable parameters.
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"{total_params:,} total parameters.")
+    total_trainable_params = sum(
+        p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"{total_trainable_params:,} training parameters.")
+
+
+
