751df2d12ba482e14b63d6c74640a1a1d17bd09d
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-01-18 19:22:26 -0600

    Changed script name

--- shell_scripts/git_commit_jacob.sh
+++ shell_scripts/git_commit_jacob.sh
@@ -1,7 +1,9 @@
 git config --global user.name "jacobollinger"
 git config --global user.email "7657565+jacobollinger@users.noreply.github.com"
 
-read -p "Press enter to when you are finished with the git push"
+read -p "Please enter the commit message: " commit_message
+
+git commit -m "$commit_message
 
 git config --unset --global user.name
 git config --unset --global user.email
