678cc65c0210c01dbf7ad41450de691f8e9b8e00
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-01-18 19:24:41 -0600

    Fixed typo

--- shell_scripts/git_commit_jacob.sh
+++ shell_scripts/git_commit_jacob.sh
@@ -3,7 +3,7 @@ git config --global user.email "7657565+jacobollinger@users.noreply.github.com"
 
 read -p "Please enter the commit message: " commit_message
 
-git commit -m "$commit_message
+git commit -m "$commit_message"
 
 git config --unset --global user.name
 git config --unset --global user.email
