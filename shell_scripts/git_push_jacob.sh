82e8411549aed2401d6930503c160b01be05e59f
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-01-18 19:19:40 -0600

    Add git_push_jacob.sh script for configuring user name and email

---
+++ shell_scripts/git_push_jacob.sh
@@ -0,0 +1,7 @@
+git config --global user.name "jacobollinger"
+git config --global user.email "7657565+jacobollinger@users.noreply.github.com"
+
+read -p "Press enter to when you are finished with the git push"
+
+git config --unset --global user.name
+git config --unset --global user.email
