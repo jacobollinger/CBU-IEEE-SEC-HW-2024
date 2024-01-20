6a95f9898d01fdfb72042728f8c4639ad3ea7ee4
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-01-19 21:42:19 -0600

    moved outputs to data/out directory

--- Python/custom_object_detection_ieee/src/config.py
+++ Python/custom_object_detection_ieee/src/config.py
@@ -4,7 +4,7 @@ GUI = False
 
 BATCH_SIZE = 30 # Increase / decrease according to GPU memeory.
 RESIZE_TO = 640 # Resize the image for training and transforms.
-NUM_EPOCHS = 10 # Number of epochs to train for.
+NUM_EPOCHS = 1000 # Number of epochs to train for.
 NUM_WORKERS = 4 # Number of parallel workers for data loading.
 
 
@@ -23,4 +23,4 @@ NUM_CLASSES = len(CLASSES)
 # whether to visualize images after crearing the data loaders
 VISUALIZE_TRANSFORMED_IMAGES = False
 # location to save model and plots
-OUT_DIR = './Python/custom_object_detection_ieee/outputs'
+OUT_DIR = './data/out/ieee'
