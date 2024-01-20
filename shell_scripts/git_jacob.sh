a1d1864d5b025e655ada5ecccb60795806b2635a
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-01-19 21:45:25 -0600

    Added script to temporarily set git user

---
+++ shell_scripts/git_jacob.sh
@@ -0,0 +1,7 @@
+git config --global user.name "jacobollinger"
+git config --global user.email "7657565+jacobollinger@users.noreply.github.com"
+
+read -p "Press enter when finished committing."
+
+git config --unset --global user.name
+git config --unset --global user.email
