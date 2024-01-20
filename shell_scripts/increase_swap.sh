42353e0b48e05e06f2a87f63f8e8773d7477eb88
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-01-19 21:43:07 -0600

    Update swap file creation and permissions

--- shell_scripts/increase_swap.sh
+++ shell_scripts/increase_swap.sh
@@ -6,8 +6,9 @@ else
     size=16
 fi
 
-sudo swapoff -a
+sudo swapoff /swfile
 sudo dd if=/dev/zero of=/swfile bs=1G count=$size
+sudo chmod 600 /swfile
 sudo mkswap /swfile
 sudo swapon /swfile
 echo '/swfile none swap sw 0 0' | sudo tee -a /etc/fstab
