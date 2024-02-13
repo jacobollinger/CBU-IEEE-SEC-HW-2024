0cc13a8fbc3acb23aad3818a62fbf48a68300b41
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-02-13 15:00:08 -0600

    Updated the arm code

--- Arduino/Arm/include/RobotArmControl.h
+++ Arduino/Arm/include/RobotArmControl.h
@@ -1,11 +1,27 @@
 #ifndef ROBOT_ARM_CONTROL_H
 #define ROBOT_ARM_CONTROL_H
 
-#include <Servo.h>
-#include <Arduino.h>
+#pragma region Arm Pins
+#define ARM_WRIST_PIN 8
+#define ARM_BASE_PIN 9
+#define ARM_SHOULDER_PIN 10
+#define ARM_ELBOW_PIN 11
+#define ARM_GRIPPER_PIN 12
+#define ARM_WRIST_FEEDBACK_PIN A0
+#define ARM_BASE_FEEDBACK_PIN A1
+#define ARM_SHOULDER_FEEDBACK_PIN A2
+#define ARM_ELBOW_FEEDBACK_PIN A3
+#define ARM_GRIPPER_FEEDBACK_PIN A4
+#pragma endregion
+
+#pragma region Arm Angles
+
+#include <Arduino.h> // Include the Arduino library
+#include <Servo.h>   // Include the Servo library
 
 // Define the JointAngles structure
-struct JointAngles {
+struct JointAngles
+{
     float base;
     float shoulder;
     float elbow;
@@ -17,26 +33,18 @@ struct JointAngles {
 };
 
 // Define the RobotArmControl class
-class RobotArmControl {
+class RobotArmControl
+{
 public:
     RobotArmControl();
-    void initialize();
-    void updatePosition(const JointAngles& angles, const String& objective);
-    JointAngles solveIK(float x, float y, float z);
-    void calibrate();
-    static constexpr int wristPin = 8;
-    static constexpr int basePin = 9;
-    static constexpr int shoulderPin = 10;
-    static constexpr int elbowPin = 11;
-    static constexpr int gripperPin = 12;
-    static constexpr int analogPinWrist = A0;
-    static constexpr int analogPinBase = A1;
-    static constexpr int analogPinShoulder = A2;
-    static constexpr int analogPinElbow = A3;
-    static constexpr int analogPinGripper = A4;
+    static void initialize();
+    static void updatePosition(const JointAngles &angles, const String &objective);
+    static JointAngles solveIK(float x, float y, float z);
+    static void calibrate();
+
     static constexpr float j1 = 5.0;
     static constexpr float j2 = 5.0;
-    static constexpr int gripString = 13;
+    static constexpr int gripString = 13; //? What is this for?
     static constexpr int gripLargePackage = 14;
     static constexpr int gripSmallPackage = 15;
     static constexpr int gripBooster = 16;
