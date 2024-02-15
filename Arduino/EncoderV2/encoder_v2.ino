c7430d8e0a611f43a3bf24522e7346c30fef308e
meestajar <156867225+meestajar@users.noreply.github.com>
2024-02-15 17:01:55 -0500

    simplified encoder_v2.ino

--- Arduino/EncoderV2/encoder_v2.ino
+++ Arduino/EncoderV2/encoder_v2.ino
@@ -3,26 +3,34 @@
 #include <PID.h>
 #include <util/atomic.h>
 
-// These are digital inputs for motor encoders
-#define ENCA_M1 1
-#define ENCB_M1 2
-#define ENCA_M2 3
-#define ENCB_M2 4
+
 // intiallize the two encoders and PID controllers store in array 
-Encoder encoder1(ENCA_M1, ENCB_M1);
-Encoder encoder2(ENCA_M2, ENCB_M2);
-Encoder enc_array [2] = {encoder1, encoder2}; 
+Encoder encoder1(2, 5);
+Encoder encoder2(3, 4);
+int ENCA_M1 = 2; 
+int ENCB_M1 = 5; 
+int ENCA_M2 = 3; 
+int ENCB_M2 = 4; 
 PID PIDcontroller1;
 PID PIDcontroller2;
 // Intialize variables
 int prev_time = 0; 
 float actual_speed1 = 0;
 float actual_speed2 = 0;
+//int pos[2];
+volatile long int left_pos = 0;
+volatile long int right_pos = 0;
 
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
+
+// These are digital inputs
+pinMode(ENCA_M1, INPUT);
+pinMode(ENCB_M1, INPUT);
+pinMode(ENCA_M2, INPUT);
+pinMode(ENCB_M2, INPUT);
 
 // Setup PID Controller parameters
 PIDcontroller1.setParameters(0.1, 0.01, 0.01);  // Adjust the gains as needed
@@ -38,36 +46,33 @@ attachInterrupt(digitalPinToInterrupt(ENCA_M2), readEncoder2, RISING);
 void loop() {
 // put your main code here, to run repeatedly:
 // intialize variables
-float current_time = millis();
+/*float current_time = millis();
 float dt = current_time - prev_time;
 
 // Define target speed(should be part of the movement/sensor code)
 // Reverse sign PWM to get them to move in same direction (make sure correct pos are read)
 // may need to change signs of motor 
 int target_speed1 = 400; 
-int target_speed2 = -400; 
+int target_speed2 = -400; */
 
-// Store positions in an array 
-int pos[2];
-ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-  pos[0] = encoder1.getPosition();
-  pos[1] = encoder2.getPosition();
-}
+left_pos = encoder1.getPosition();
+right_pos = encoder2.getPosition();
+
 // Get actual speed (need to fix)
-actual_speed1 = encoder1.speedInRPM(pos[0]);
-actual_speed2 = encoder2.speedInRPM(pos[1]);
+actual_speed1 = encoder1.speedInRPM(left_pos);
+actual_speed2 = encoder2.speedInRPM(right_pos);
 // Call PID function 
-
+/*
 PIDcontroller1.calculateNewSignal(target_speed1, actual_speed1);
 PIDcontroller2.calculateNewSignal(target_speed2, actual_speed2); 
-// Set Motor send the corrected signal to the motor; need motor driver 
+// Set Motor send the corrected signal to the motor; need motor driver */
 
 Serial.print("Motor 1 Position: ");
-Serial.println(pos[0]);
-
+Serial.println(left_pos);
+Serial.println();
 Serial.print("Motor 2 Position: ");
-Serial.println(pos[1]);
-
+Serial.println(right_pos);
+Serial.println();//
 }
 
 // Interrupt service routines for encoders
