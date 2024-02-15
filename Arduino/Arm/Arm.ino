dcf549710deaf5814613f383aee3dcd94f66993b
meestajar <156867225+meestajar@users.noreply.github.com>
2024-02-14 20:15:58 -0500

    added simple test using serial monitor

--- Arduino/Arm/Arm.ino
+++ Arduino/Arm/Arm.ino
@@ -1,9 +1,15 @@
-#include "./include/RobotArmControl.h"
+#include <RobotArmControl.h>
+#include <Servo.h>
+Servo testServo;
 
 void setup()
-{
+
+{   RobotArmControl test;
     Serial.begin(115200); // Initialize serial communication
-    RobotArmControl::initialize(); // Initialize the robot arm
+   // test.initialize(); // Initialize the robot arm
+   
+    // Attach the servo to pin x
+    testServo.attach(10);
 }
 
 void loop()
@@ -23,11 +29,39 @@ void loop()
 
     while (!Serial.available()) {}
 
-    // input format: "x y z"
+   /* // input format: "x y z"
     String input = Serial.readString();
     float x = input.substring(0, input.indexOf(' ')).toFloat();
     input = input.substring(input.indexOf(' ') + 1);
     float y = input.substring(0, input.indexOf(' ')).toFloat();
     input = input.substring(input.indexOf(' ') + 1);
-    float z = input.toFloat();
+    float z = input.toFloat();*/
+
+    // input format: "x y z"
+ 
+    String input = Serial.readString();
+    int spaceIndex = input.indexOf(' ');
+    int spaceIndex2 = input.lastIndexOf(' '); 
+    
+    if (spaceIndex != -1 && spaceIndex2 != -1) {
+    // Get the substring before the space as x
+    int x = input.substring(0, spaceIndex).toInt();
+    Serial.println(x); 
+    
+    // Get the substring after the space as y
+    int y = input.substring(spaceIndex + 1).toInt();
+    Serial.println(y); 
+// Read Anloag output at this location   
+// get the substring after the space as z 
+//    int z = input.substring(spaceIndex2 + 1).toInt(); 
+    
+    
+    // Set the servo position to y degrees
+    testServo.write(y);
+
+//    if( z != 0); {
+//    int output = analogRead(z); 
+//    Serial.println(output); 
+//    }
+  }
 }
