a000c1bc028577d1a76cdbea57d46d6c674b173b
jacobollinger <7657565+jacobollinger@users.noreply.github.com>
2024-02-21 14:05:00 -0600

    updated defines

--- Arduino/Main/include/Defines.hpp
+++ Arduino/Main/include/Defines.hpp
@@ -4,6 +4,40 @@
 #define DEBUG true
 #define BAUDRATE 115200
 
+#pragma region Wheel Definitions
+#define WHEELS_M1_FLT 33
+#define WHEELS_M1_PWM 8
+#define WHEELS_M1_SLP 30
+#define WHEELS_M1_DIR 25
+#define WHEELS_M1_CS A15
+#define WHEELS_M2_FLT 31
+#define WHEELS_M2_PWM 7
+#define WHEELS_M2_SLP 32
+#define WHEELS_M2_DIR 23
+#define WHEELS_M2_CS A14
+
+#define WHEEL_MAX_SPEED 255
+
+#pragma endregion
+
+// TODO: Add arm definitions
+#pragma region ADC Definitions
+#define ARM_BASE_PIN
+#define ARM_SHOULDER_PIN
+#define ARM_WRIST_PIN
+#define ARM_GRIPPER_PIN
+
+#define ARM_SHOULDER_FEEDBACK_PIN
+#define ARM_WRIST_FEEDBACK_PIN
+#define ARM_GRIPPER_FEEDBACK_PIN
+
+#define ARM_GRIPPER_OPEN
+#define ARM_GRIPPER_GRIP_FUEL_TANK
+#define ARM_GRIPPER_GRIP_SMALL_PACKAGE
+#define ARM_GRIPPER_GRIP_LARGE_PACKAGE
+
+#pragma endregion
+
 #define SPI_EOT 0x04
 #define SPI_BUFFER_SIZE 1UL << 8
 
