dd3c21e243fe8c4389fc628fc525cfd49a67c002
meestajar <156867225+meestajar@users.noreply.github.com>
2024-02-23 22:29:20 -0500

    Updated functions

--- Arduino/Arm/include/RobotArmControl.h
+++ Arduino/Arm/include/RobotArmControl.h
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include <Servo.h>
+#include <Math.h>
 
 struct JointAngles
 {
@@ -20,44 +21,44 @@ class RobotArmControl
 public:
     RobotArmControl();
     void initialize();
+    int angleToMicroseconds360(double angle);
+    int calcVectorAngle(double x, double y); // Calculate angle of vector
     void moveToAngle(double x, double y, double z, double g);
     void updatePosition(double x, double y, double z, double g);
-    void solveIK(float x, float y, float z);
+    void solveIK(double x, double y, double z);
     void calibrate();    
 
 
 
 private:
 	
-Servo baseServo;
-Servo shoulderServo;
-Servo wristServo;
-Servo gripperServo;
-
-JointAngles dropOffAnglesLargePkg;
-JointAngles dropOffAnglesSmallPkg;
-JointAngles dropOffAnglesBoosters;
-JointAngles dropBridgeAngles;
-JointAngles initializedAngles;
-JointAngles currentAngles;
+    Servo baseServo;
+    Servo shoulderServo;
+    Servo wristServo;
+    Servo gripperServo;
+    JointAngles dropOffAnglesLargePkg;
+    JointAngles dropOffAnglesSmallPkg;
+    JointAngles dropOffAnglesBoosters;
+    JointAngles dropBridgeAngles;
+    JointAngles initializedAngles;
+    JointAngles currentAngles;
 	
-const float L0 = 19.2875; // length from ground to second joint (elbow)
-const float L1 = 12.85875; // length from 2nd joint to 3rd joint (elbow to wrist)
-const float L2 = 10.16; // length from 3rd joint to gripper tip
-const float pi = 3.141592653589793;
+	const float L0 = 16.66875; // length from ground to second joint (shoulder)
+        const float L1 = 13.0175; // length from 2nd joint to 3rd joint (shoulder to wrist)
+	const float L2 = 12.7; // length from 3rd joint to gripper tip
+	const float pi = 3.141592653589793;
 	
 	//const int gripString;
 	//const int gripLargePackage;
 	//const int gripSmallPackage;
 	//const int gripBooster;
 	//const int gripRelease;
-	//const float pi; 
 	//int g;
 	
-// Analog inputs
-int ARM_GRIPPER_FEEDBACK_PIN;
-int ARM_WRIST_FEEDBACK_PIN; 
-int ARM_SHOULDER_FEEDBACK_PIN;
+	// Analog inputs
+    int ARM_GRIPPER_FEEDBACK_PIN;
+	int ARM_WRIST_FEEDBACK_PIN; 
+	int ARM_SHOULDER_FEEDBACK_PIN;
 
 };
 
