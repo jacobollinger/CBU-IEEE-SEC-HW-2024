6d5c7647465acb8bdb69c4d45cfe262583080611
meestajar <156867225+meestajar@users.noreply.github.com>
2024-03-01 16:03:24 -0500

    Update RobotArmControl.h

--- Arduino/Arm/include/RobotArmControl.h
+++ Arduino/Arm/include/RobotArmControl.h
@@ -5,58 +5,47 @@
 #include <Servo.h>
 #include <Math.h>
 
-struct JointAngles
-{
-    float base;
-    float shoulder;
-    float wrist;
-    float gripper;
-
-    JointAngles(float initBase = 90.0, float initShoulder = 90.0, float initWrist = 90.0, float initGripper = 90.0)
-        : base(initBase), shoulder(initShoulder), wrist(initWrist), gripper(initGripper) {}
-};
-
-class RobotArmControl
-{
+class RobotArmControl{
 public:
     RobotArmControl();
-    void initialize();
+	void initialize();
 	int angleToMicroseconds360(double angle);
 	int calcVectorAngle(double x, double y); // Calculate angle of vector
 	void moveToAngle(double x, double y, double z, double g);
-    void updatePosition(double x, double y, double z, double g);
-    void solveIK(double x, double y, double z);
-    void calibrate();    
-
-
-
+	//void updatePosition(double x, double y, double z, double g);
+	void updatePosition(String objective);
+	void solveIK(double x, double y, double z);
+	void calibrate();    
 private:
 	
-    Servo baseServo;
-    Servo shoulderServo;
+   	Servo baseServo;
+    	Servo shoulderServo;
 	Servo wristServo;
 	Servo gripperServo;
-    JointAngles dropOffAnglesLargePkg;
-	JointAngles dropOffAnglesSmallPkg;
-	JointAngles dropOffAnglesBoosters;
-    JointAngles dropBridgeAngles;
-    JointAngles initializedAngles;
-    JointAngles currentAngles;
+	
+// Predefined Angles *Determine these
+	double gripLargePackage = 46;
+	double gripSmallPackage = 35; 
+	double gripBooster = 18; 
+	double gripRelease = 88; 
+	
+	// Store specific angles in arrays
+        double dropBridgeAngles[4] = {90, 90, 90, 90};      // set angles for droppping off bridge *update
+        double initializedAngles[4] = {888, 177.56, 0, 88};     // set angles for statup *update
+	double currentAngles[4] = {90, 90, 90, 90}; // 
+	double dropOffAnglesLargePkg[4] = {1666, 140, 11.76, 46}; // set angles for dropping off large packages *update
+        double dropOffAnglesSmallPkg[4] = {2194, 131, 11.76, 35}; // set angles for dropping off small packages * updateSerial.print("dropOffAnglesSmallPkg.base: ");
+	double dropOffAnglesBoosters[4] = {90, 90, 90, 18}; // set angles for dropping off boosters *update) {}
 	
 	const float L0 = 10.125; // length from ground to second joint (shoulder)
-    const float L1 = 18.25625; // length from 2nd joint to 3rd joint (shoulder to wrist)
+        const float L1 = 18.25625; // length from 2nd joint to 3rd joint (shoulder to wrist)
 	const float L2 = 12.7; // length from 3rd joint to gripper tip
 	const float pi = 3.141592653589793;
 	
-	//const int gripString;
-	//const int gripLargePackage;
-	//const int gripSmallPackage;
-	//const int gripBooster;
-	//const int gripRelease;
 	//int g;
 	
 	// Analog inputs
-    int ARM_GRIPPER_FEEDBACK_PIN;
+        int ARM_GRIPPER_FEEDBACK_PIN;
 	int ARM_WRIST_FEEDBACK_PIN; 
 	int ARM_SHOULDER_FEEDBACK_PIN;
 
