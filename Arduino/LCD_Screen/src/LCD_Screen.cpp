64380b0af855e47cfccd2a25728f0b55fe30c77f
jodierea <158482156+jodierea@users.noreply.github.com>
2024-03-18 19:08:45 -0500

    Update LCD_Screen.cpp

--- Arduino/LCD_Screen/src/LCD_Screen.cpp
+++ Arduino/LCD_Screen/src/LCD_Screen.cpp
@@ -2,7 +2,7 @@
 #include "../include/LCD_Screen.hpp"
 
 // Initialize your object with the necessary pin numbers
-LCD_Screen::LCD_Screen() : promoLCD(12, 11, 5, 4, 3, 2) {
+LCD_Screen::LCD_Screen() : promoLCD(42, 43, 44, 45, 46, 47) {
 }
 
 void LCD_Screen::init() {
